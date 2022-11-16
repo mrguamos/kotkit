69d67380200c95dd5b6655c4cc58794b55b5d815
Mark Ryan Guamos <ryan.guamos@gmail.com>
2022-11-16 08:50:51 +0800

    prop

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
       meta: [
         { name: 'description', content: 'Nuxt3 TikTok Downloader' },
         { name: 'propeller', content: '71cf8ce19fcabe2618d829817bb5b22a' },
+        { name: 'propeller', content: '1e6f51485f194e44ddc28567e2a28e95' },
       ],
       script: [
         {
