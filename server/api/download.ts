bfb3acaa3b80e33c60ea97439c9b527c39c11753
Mark Ryan Guamos <ryan.guamos@gmail.com>
2023-01-15 21:04:43 +0800

    add url condition

--- server/api/download.ts
+++ server/api/download.ts
@@ -1,20 +1,22 @@
 import type { RootObject } from './../../types/tiktok'
 export default eventHandler(async (event) => {
-  let path = ''
   const paramURL: string = getQuery(event).url?.toString() || ''
-  await $fetch(paramURL, {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Connection': 'keep-alive',
-    },
-    onResponse: ({ response }) => {
-      path = response.url.split(/[?#]/)[0]
-    },
-  })
+  let path = paramURL.split(/[?#]/)[0]
   if (path.includes('tiktok')) {
+    if (!path.includes('video')) {
+      await $fetch(paramURL, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Accept-Language': 'en-US,en;q=0.9',
+          'Connection': 'keep-alive',
+        },
+        onResponse: ({ response }) => {
+          path = response.url.split(/[?#]/)[0]
+        },
+      })
+    }
     const videoId = path.substring(path.lastIndexOf('/') + 1, path.length)
     if (videoId) {
       const url = 'https://api.tiktokv.com/aweme/v1/feed/'
