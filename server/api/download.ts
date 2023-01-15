60dfdd59d4e6fe119b82d221846076f0c52f3014
Mark Ryan Guamos <ryan.guamos@gmail.com>
2023-01-15 19:42:51 +0800

    filename

--- server/api/download.ts
+++ server/api/download.ts
@@ -12,7 +12,7 @@ export default eventHandler(async (event) => {
   })
   const video = await $fetch<Blob>(data?.aweme_list[0].video.play_addr.url_list[0] ?? '')
   const arrayBuffer = await video.arrayBuffer()
-  event.node.res.setHeader('Content-Disposition', 'attachment; filename=download.mp4')
+  event.node.res.setHeader('Content-Disposition', `attachment; filename=${videoId}.mp4`)
   event.node.res.setHeader('Content-Type', event.node.req.headers['content-type'] || '')
   event.node.res.writeHead(200)
   event.node.res.write(Buffer.from(arrayBuffer))
