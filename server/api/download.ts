fcfd8aef5f73896872c035d051bf7a5bb4b11837
Mark Ryan Guamos <ryan.guamos@gmail.com>
2023-01-15 22:54:17 +0800

    code cleanup

--- server/api/download.ts
+++ server/api/download.ts
@@ -4,7 +4,7 @@ export default eventHandler(async (event) => {
   let path = paramURL.split(/[?#]/)[0]
   if (path.includes('tiktok')) {
     if (!path.includes('video')) {
-      await $fetch(paramURL, {
+      await $fetch(path, {
         headers: {
           'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
